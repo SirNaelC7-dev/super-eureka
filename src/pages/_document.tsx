60bff966bfe3683b056d3e281710c11d0fbe817a
Pedro Costa <devpedrohenrique@gmail.com>
2022-03-05 01:31:13 -0300

    feat: adicionado tela inicial

---
+++ src/pages/_document.tsx
@@ -0,0 +1,23 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+import theme from '../styles/theme'
+import { ColorModeScript } from '@chakra-ui/react'
+
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Caveat:wght@700&display=swap"
+          rel="stylesheet"
+        />
+        <title>Eureka</title>
+      </Head>
+      <body>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import Header from '../components/home/Header'
+import Footer from '../components/shared/Footer'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  )
+}
+
+export default Home
