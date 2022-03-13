2ed5edf7bc7f9e1318fc177d662e95a15a92eaad
Pedro Costa <devpedrohenrique@gmail.com>
2022-03-13 11:49:09 -0300

    feat: adicionado função para calcular os valores de área

--- src/components/categories/Dimension/Area/index.tsx
+++ src/components/categories/Dimension/Area/index.tsx
@@ -4,13 +4,13 @@ import {
     Container,
     Stack,
     Flex,
-    Spacer,
     FormControl,
     FormLabel,
     Input,
     Select,
-    Button,
-    Icon,
+    StatLabel,
+    StatNumber,
+    Stat,
 
 } from '@chakra-ui/react';
 
@@ -24,23 +24,82 @@ interface MedidaProps {
     key: string,
 }
 
+const metroQuadradoToCentimetroQuadrado: (metros: number) => number = (metros) => {
+    return metros * 10000;
+}
+
+const metroQuadradoToMilimetroQuadrado: (metros: number) => number = (metros) => {
+    return metros * 1000000;
+}
+
+const metroQuadradoToQuilometroQuadrado: (metros: number) => number = (metros) => {
+    return metros * 0.000001;
+}
+
+
+
+
+const centimetroQuadradoToMetroQuadrado: (centrimetros: number) => number = (centrimetros) => {
+    return centrimetros / 10000;
+}
+
+const milimetroQuadradoToMetroQuadrado: (milimetro: number) => number = (milimetro) => {
+    return milimetro / 1000000;
+}
+
+const quilometroQuadradoToMetroQuadrado: (quilometro: number) => number = (quilometro) => {
+    return quilometro / 0.000001;
+}
+
 const medida = 'Área';
 const definicao = 'Área é um conceito matemático que pode ser definida como quantidade de espaço bidimensional, ou seja, de superfície. ';
 const medidas: MedidaProps[] = [
     {name:'', key:''},
-    {name:'Quilômetro Quadrado', key: 'quilometroQ'},
-    {name:'Hectare', key:'hectare'},
-    {name:'Are', key:'are'},
-    {name:'Metro Quadrado', key: 'metroQ'},
-    {name:'Centímetro Quadrado', key: 'centimetroQ'},
-    {name:'Milímetro Quadrado', key: 'milimetroQ'},
-    {name:'Decímetro Quadrado', key: 'decimetroQ'},
-    {name:'Micrômetro Quadrado', key: 'micrometroQ'},
-    {name:'Nanometro Quadrado', key: 'nanometroQ'}
+    {name:'Quilômetro Quadrado', key: 'quilometroQuadrado'},
+    // {name:'Hectare', key:'hectare'},
+    // {name:'Are', key:'are'},
+    // {name:'Metro Quadrado', key: 'metroQuadrado'},
+    {name:'Centímetro Quadrado', key: 'centimetroQuadrado'},
+    {name:'Milímetro Quadrado', key: 'milimetroQuadrado'},
+    // {name:'Decímetro Quadrado', key: 'decimetroQuadrado'},
+    // {name:'Micrômetro Quadrado', key: 'micrometroQuadrado'},
+    // {name:'Nanometro Quadrado', key: 'nanometroQuadrado'}
 ];
+
+const parsesToMetro = {
+    quilometroQuadrado: quilometroQuadradoToMetroQuadrado,
+    centimetroQuadrado: centimetroQuadradoToMetroQuadrado,
+    milimetroQuadrado: milimetroQuadradoToMetroQuadrado
+}
+
+const parsesToFinalValue = {
+    quilometroQuadrado: metroQuadradoToQuilometroQuadrado,
+    centimetroQuadrado: metroQuadradoToCentimetroQuadrado,
+    milimetroQuadrado: metroQuadradoToMilimetroQuadrado,
+}
 
 const Area: NextComponentType = () => {
     const [value, setValue] = useState<number | ''>(0);
+    const [valueParseToMetro, setValueParseToMetro] = useState<number>(0);
+    const [metroToFinalValue, setMetroToFinalValue] = useState<number>(0);
+
+
+    const parseToMetro = (key: string) => {
+        if(parsesToMetro[key]){
+            const metroValue = parsesToMetro[key](value || 0)
+            setValueParseToMetro(metroValue)
+        }
+    }
+
+
+    const parseToFinalValue = (key: string) => {
+        if(parsesToFinalValue[key]){
+            const finalValue = parsesToFinalValue[key](valueParseToMetro)
+            setMetroToFinalValue(finalValue)
+        }
+    }
+
+
     return(
         <Container maxW={'md'}>
             <Stack spacing={'5'}>
@@ -56,7 +115,7 @@ const Area: NextComponentType = () => {
                     <FormLabel color={'gray.600'}>Digite abaixo o valor para converter</FormLabel>
                     <Input id='num' type='number' value={value} onChange={(e)=>setValue(Number(e.target.value) || '')}/>
                     <FormLabel color={'gray.600'}>Selecione a medida em que está o valor</FormLabel>
-                    <Select id='tipodado'>
+                    <Select id='tipodado' onChange={(e) => parseToMetro(e.target.value)}>
 
                         {medidas.map((item)=>{
                             return(
@@ -66,7 +125,7 @@ const Area: NextComponentType = () => {
 
                     </Select>
                     <FormLabel color={'gray.600'}>Selecione a medida que quer como resultado</FormLabel>
-                    <Select id='tiporesultado' >
+                    <Select id='tiporesultado' onChange={(e) => parseToFinalValue(e.target.value)}>
                         
                         {medidas.map((item)=>{
                             return(
@@ -82,21 +141,11 @@ const Area: NextComponentType = () => {
                     align={'center'}
                     flexDirection={'row-reverse'}
                     >
-                        <Stack>
-
-                            <Text
-                            color={'blue.400'}>
-                                <b>Resultado: XXX</b>
-                            </Text>
-                        </Stack>
-                        <Spacer/>
-                        <Stack>
-                            <Button color={'blue.400'}>
-                                <Icon color={'blue.400'}>
-                                    <ArrowRightIcon />
-                                </Icon>
-                            </Button>
-                        </Stack>
+                        <Stat>
+                            <StatLabel>Resultado</StatLabel>
+                            <StatNumber>{metroToFinalValue}</StatNumber>
+                            {/* <StatHelpText>Feb 12 - Feb 28</StatHelpText> */}
+                        </Stat>
                     </Flex>
                 </FormControl>       
             </Stack>
@@ -105,14 +154,3 @@ const Area: NextComponentType = () => {
 }
 
 export default Area;
-
-const metroToCentimetro: (metros: number) => number = (metros) => {
-    return metros * 100;
-}
-
-const metroToMilimetro: (metros: number) => number = (metros) => {
-    return metros * 1000;
-}
-
-
-
